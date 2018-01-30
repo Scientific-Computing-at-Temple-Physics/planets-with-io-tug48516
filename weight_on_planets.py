581340a63f2f981000b900a168f764f719884ef8
M Newby <matthew.t.newby@gmail.com>
2018-01-29 21:44:50 -0500

    Create weight_on_planets.py

---
+++ weight_on_planets.py
@@ -0,0 +1,4 @@
+# This is a comment.  Python will ignore these lines (starting with #) when running
+
+import math as ma
+# To use a math function, write "ma." in front of it.  Example:  ma.sin(3.146)
