589b634ccd4b9642e9b1953b33c7a1b3357fc3fd
tug48516 <35577358+tug48516@users.noreply.github.com>
2018-02-06 15:10:00 -0500

    Add files via upload

--- weight_on_planets.py
+++ weight_on_planets.py
@@ -2,3 +2,15 @@
 
 import math as ma
 # To use a math function, write "ma." in front of it.  Example:  ma.sin(3.146)
+
+#planet of interest: Jupiter
+# 100 m above average Radius
+# Explorer=60kg
+# Jupiter;  69911 km;  1.326 g/cm^3;  gas giant
+# m=mass of explorer
+import math as ma
+V=(4*ma.pi*(69911*1000)**3)/3.
+M=1.326*V
+G=((6.67*10**-11)*M*60)/((69911000+100)**2)
+F=G*60
+print (F,G)
