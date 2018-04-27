cd82ec44ed597868f89ac5083277768f4e7ad147
Siyeon Hwang <tug48516@temple.edu>
2018-04-27 16:02:23 -0400

    weight progressing

--- weight_on_planets.py
+++ weight_on_planets.py
@@ -2,15 +2,35 @@
 
 import math as ma
 # To use a math function, write "ma." in front of it.  Example:  ma.sin(3.146)
+#need to find the explorer's weight and gravitational acceleration
+#data = open("planet_data.dat","r")
 
-#planet of interest: Jupiter
-# 100 m above average Radius
-# Explorer=60kg
-# Jupiter;  69911 km;  1.326 g/cm^3;  gas giant
-# m=mass of explorer
-import math as ma
-V=(4*ma.pi*(69911*1000)**3)/3.
-M=1.326*V
-G=((6.67*10**-11)*M*60)/((69911000+100)**2)
-F=G*60
-print (F,G)
+planet = ["Mercury","Venus","Earth","Mars","Ceres","Jupiter","Saturn","Uranus","Neptune","Pluto","Eris","Comet","Proxima","Kepler-37"]
+# r is average radius in km
+radius = [2439.7,6051.8,6371.0,3389.5,473,69911,58232,25362,24622,1188.3,1163,3.7,8000.0,2176.0]
+#Radius is in m
+Radius = [i*1000 for i in radius]
+
+Density = [5.427,5.243,5.514,3.933,2.161,1.326,0.687,1.27,1.638,1.854,2.52,0.533,3.636,5.560]
+
+import random
+# we will use random number generator to chose our planet
+#n is planet number
+n=random.random()*13
+m = (float(n))
+#p is randomply selected planet
+p = planet [m]
+
+
+print p
+#r is randomly selected planet's radius
+
+V =4./3.*ma.pi*n**3
+# M is mass of an object, given its density and volume
+
+
+# G is gravity constant
+G = 6.667*(10**(-11))
+
+# m is explorer's mass in kg
+m = 50
